311a48ad246ad83ffd7f7ef1c974032fb8cf675a
Fraser <fdonnelly82@gmail.com>
2015-10-19 14:54:45 +0100

    Hello

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <p>
+            <?php
+            echo    "Hello World";
+            ?>
+        </p>
+    </body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: Fraser
+ * Date: 19/10/2015
+ * Time: 14:47
+ */
