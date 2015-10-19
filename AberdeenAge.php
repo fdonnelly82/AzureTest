db213f8683ac8e04b2a41b2c8695d8f5873771e6
Fraser <fdonnelly82@gmail.com>
2015-10-19 15:25:23 +0100

    Test 2

---
+++ AberdeenAge.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <p>
+            <?php
+             $name = "Fraser";
+             $myAge = 19;
+
+             if ($myAge >= 16) {
+                 print $name + " can buy specs.";
+             }
+             elseif ($myAge < 16) {
+                 print $name + " cannot by specs.";
+             }
+
+            elseif ($myAge >= 18) {
+                print $name + " can buy mugs.";
+            }
+
+            elseif ($myAge < 18) {
+                print $name + " cannot buy mugs.";
+            }
+
+            elseif ($myAge >= 21) {
+                print $name + " can buy sausage rolla.";
+            }
+
+            elseif ($myAge < 21) {
+                print $name + " cannot buy sausage rolls.";
+            }
+            ?>
+        </p>
+    </body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: Fraser
+ * Date: 19/10/2015
+ * Time: 14:59
+ */
