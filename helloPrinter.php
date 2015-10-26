43499510ee5c3c20a6764395c7d29da766ce2004
Fraser <fdonnelly82@gmail.com>
2015-10-26 16:29:37 +0000

    Hello again 2

---
+++ helloPrinter.php
@@ -0,0 +1,21 @@
+<!DOCTYPE = html>
+<html>
+<head>
+
+</head>
+<body>
+    <p>
+        <?php
+        $query = $_GET["query"];
+
+        echo "Hello " . $query;
+        ?>
+    </p>
+</body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: Fraser
+ * Date: 26/10/2015
+ * Time: 15:19
+ */
