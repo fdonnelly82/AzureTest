ab04a6b8e63d9b3a7c8c3770b0ed354fb731b0c4
1302264 <fdonnelly82@gmail.com>
2015-11-30 15:57:32 +0000

    Hello again 2,ft twitter incorperation

--- twitterincorperation.php
+++ twitterincorperation.php
@@ -5,7 +5,7 @@
     <title>Consuming Twitter Webserver</title>
     <script>
         window.onload = function() {
-            var xhttp = XMLHttpRequest();
+            var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (xhttp.readyState == 4 && xhttp.status == 200){
                     document.getElementById("twitter").innerHTML = xhttp.responseText;
